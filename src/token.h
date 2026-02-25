efcea06742b6e71eaf443ea400e12005582538f4
SMKM <svargas@desma.com.mx>
2026-02-24 21:03:37 -0600

    configs

---
+++ src/token.h
@@ -0,0 +1,15 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+
+namespace tkn
+{
+
+// consts to identify the tokens - keywords, identifiers,
+// operators
+const std::string ENDOF{"EOF"};
+
+} // namespace tkn
+
+#endif // TOKEN_H
